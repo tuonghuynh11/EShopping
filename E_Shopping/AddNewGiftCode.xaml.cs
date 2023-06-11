af0d0090dfad44b3e87875b237a9661737b00fca
tuonghuynh11 <89913828+tuonghuynh11@users.noreply.github.com>
2023-06-11 23:53:28 +0700

    add ManageGiftCode new

---
+++ E_Shopping/AddNewGiftCode.xaml.cs
@@ -0,0 +1,99 @@
+﻿using E_Shopping.Model;
+using E_Shopping.PopUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace E_Shopping
+{
+    /// <summary>
+    /// Interaction logic for AddNewGiftCode.xaml
+    /// </summary>
+    public partial class AddNewGiftCode : Window
+    {
+        public int idCustomer { get; set; }
+        public AddNewGiftCode()
+        {
+            InitializeComponent();
+        }
+        public AddNewGiftCode(int idCustomer)
+        {
+            InitializeComponent();
+            this.idCustomer = idCustomer;
+        }
+
+        private void Close_but_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (newCodetb.Text.ToString()=="")
+            {
+                ValidationNotify validation = new ValidationNotify("Code is empty");
+                validation.ShowDialog();
+                return;
+            }
+            else if (discountValuetb.Text.ToString()=="")
+            {
+                ValidationNotify validation = new ValidationNotify("Discount value is empty");
+                validation.ShowDialog();
+                return;
+            }
+            else if (expDate.SelectedDate==null)
+            {
+                ValidationNotify validation = new ValidationNotify("Expire day is empty");
+                validation.ShowDialog();
+                return;
+            }
+
+
+            String newCode = newCodetb.Text.ToString();
+            
+            int number;
+            if (!int.TryParse(discountValuetb.Text.ToString(),out number))
+            {
+                ValidationNotify validation = new ValidationNotify("Discount value musts number");
+                validation.ShowDialog();
+                return;
+            }
+            int percent = int.Parse(discountValuetb.Text.ToString());
+
+            DateTime exp = expDate.SelectedDate.Value;
+            if (expDate.SelectedDate.Value< DateTime.Today)
+            {
+                ValidationNotify validation = new ValidationNotify("Expire day shouldn't less than today");
+                validation.ShowDialog();
+                return;
+            }
+
+           DataProvider.ins.db.DISCOUNTs.Add(new DISCOUNT() { idCustomer= this.idCustomer,code =newCode, value= percent, exp_time= exp ,status="Unused" });
+            DataProvider.ins.db.SaveChanges();
+
+            this.Close();
+            SucceedNotify succeed = new SucceedNotify();
+            succeed.Show();
+        }
+        public string generateID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+        private void generateCode_Click(object sender, RoutedEventArgs e)
+        {
+            newCodetb.Text = generateID();
+        }
+    }
+}
---
+++ E_Shopping/ViewModel/ManageGiftCodeViewModel.cs
@@ -0,0 +1,72 @@
+﻿using E_Shopping.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace E_Shopping.ViewModel
+{
+    public class ManageGiftCodeViewModel:BaseViewModel
+    {
+        private ObservableCollection<Person> _personList;
+        public ObservableCollection<Person> PersonList { get => _personList; set { _personList = value; OnPropertyChanged(); } }
+
+        private Person _selectedPerson;
+        public Person SelectedPerson { get => _selectedPerson; set { _selectedPerson = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<DISCOUNT> _discountList;
+        public ObservableCollection<DISCOUNT> DiscountList { get => _discountList; set { _discountList = value; OnPropertyChanged(); } }
+
+
+
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand SelectionChangedCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        public ManageGiftCodeViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<UserControl>((p) => { return true; }, (p) => { LoadPersonData(); });
+            SelectionChangedCommand = new RelayCommand<DataGrid>((p) => { return true; }, (p) => {
+                DiscountList = new ObservableCollection<DISCOUNT>();
+                if (SelectedPerson != null)
+                    LoadDiscountData(SelectedPerson);
+            });
+            ConfirmCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                Person temp = SelectedPerson;
+                if (temp != null)
+                {
+                    AddNewGiftCode addNewGiftCode = new AddNewGiftCode(temp.id);
+                    addNewGiftCode.ShowDialog();
+                    LoadDiscountData(temp);
+                }
+
+
+            });
+            CancelCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                
+
+            });
+        }
+        void LoadPersonData()
+        {
+            PersonList = new ObservableCollection<Person>(DataProvider.ins.db.PEOPLE);
+            if (PersonList.Count!=0)
+            {
+                SelectedPerson = PersonList[0];
+                LoadDiscountData(SelectedPerson);
+            }
+
+        }
+      
+        void LoadDiscountData(Person co)
+        {
+            DiscountList = new ObservableCollection<DISCOUNT>(DataProvider.ins.db.DISCOUNTs.Where(p=>p.idCustomer==co.id));
+
+        }
+    }
+}
+
